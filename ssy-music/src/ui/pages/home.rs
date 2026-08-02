b7aeeb46564db90b35fb335a3ed5f96105143cb9
EternityShen <3258551611@qq.com>
2026-08-03 00:48:52 +0800

    update:home页面增加配置路径显示

--- ssy-music/src/ui/pages/home.rs
+++ ssy-music/src/ui/pages/home.rs
@@ -2,14 +2,19 @@
 
 pub struct HomePage {
     title: String,
+    help: String,
 }
 
 pub enum HomePageMessage {}
 
 impl HomePage {
     pub fn new() -> Self {
+        let mut user_config_dir = dirs::config_dir().ok_or("找不到系统配置目录").unwrap();
+        user_config_dir.push("ssy-music");
+
         Self {
             title: "Welcome! Syy-Music".to_string(),
+            help: user_config_dir.to_string_lossy().to_string(),
         }
     }
 
@@ -20,13 +25,24 @@ impl HomePage {
     pub fn view(&self) -> iced::Element<'_, HomePageMessage> {
         let title = iced::widget::container(iced::widget::text(self.title.clone()).size(60))
             .align_x(iced::Alignment::Center)
-            .center_y(iced::Length::Fill)
             .width(iced::Length::Fill);
 
-        iced::widget::column![title]
-            .width(iced::Length::Fill)
-            .height(iced::Length::Fill)
-            .into()
+        let help = iced::widget::container(
+            iced::widget::text(format!("配置文件和本地数据库在:{}", self.help)).size(20),
+        )
+        .align_x(iced::Alignment::Center)
+        .width(iced::Length::Fill);
+
+        iced::widget::column![
+            iced::widget::space::vertical(),
+            title,
+            help,
+            iced::widget::space::vertical()
+        ]
+        .spacing(10)
+        .width(iced::Length::Fill)
+        .height(iced::Length::Fill)
+        .into()
     }
 }
 
